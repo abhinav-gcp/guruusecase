434fa11b4e6e30646e73244b415f0034a97d6faf
abhinav <abhinavsingh8477@gmail.com>
2024-07-05 09:27:34 +0000

    final

--- terraform/apache.tf
+++ terraform/apache.tf
@@ -1,35 +1,32 @@
-# apache.tf
-
 resource "google_compute_instance_template" "apache_template" {
-  name          = "apache-template"
-  machine_type  = "e2-medium"
+  name = "apache-template"
+  machine_type = "e2-medium"
   disk {
-    source_image = "projects/${var.project_id}/global/images/apache-image"
-    auto_delete  = true
+    source_image = "gcr.io/amplified-asset-426508-e7/apache-image"
+    auto_delete = true
   }
   network_interface {
     network = var.network
-    subnetwork = var.subnetwork
     access_config {}
   }
   metadata_startup_script = <<-EOF
     #!/bin/bash
-    docker run -d -p 80:80 ${var.apache_image}:latest
+    docker run -d -p 80:80 gcr.io/amplified-asset-426508-e7/apache-image:latest
   EOF
 }
 
 resource "google_compute_instance_group_manager" "apache_group" {
-  name               = "apache-group"
+  name = "apache-group"
   base_instance_name = "apache"
-  zone               = var.zone
+  zone = var.zone
   version {
     instance_template = google_compute_instance_template.apache_template.id
   }
-  target_size        = 1
+  target_size = 1
   update_policy {
-    type                  = "PROACTIVE"
-    minimal_action        = "REPLACE"
-    max_surge_fixed       = 1
+    type = "PROACTIVE"
+    minimal_action = "REPLACE"
+    max_surge_fixed = 1
     max_unavailable_fixed = 0
   }
 }
